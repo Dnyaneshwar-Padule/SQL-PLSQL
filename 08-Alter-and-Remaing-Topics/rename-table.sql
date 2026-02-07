6d6c6f8f3b940aa6f32766ef6b93ae51e0418028
Dnyaneshwar-Padule <dnyaneshwarpadule83@gmail.com>
2026-02-07 14:36:09 +0530

    Alter and Renaming Topics: rename-table

---
+++ 08-Alter-and-Remaing-Topics/rename-table.sql
@@ -0,0 +1,27 @@
+/* 
+
+    We can rename a table with ALTER command
+*/
+
+    /*
+        MySQL: 
+
+        * Syntax - 
+            ALTER TABLE current_table_name
+            RENAME TO new_table_name;
+    */
+
+    /*
+        PostgreSQL:
+
+        * Syntax -
+            ALTER TABLE current_table_name
+            RENAME TO new_table_name;
+
+    */
+
+
+    /* Example */
+
+    ALTER TABLE student
+    RENAME to stu;
